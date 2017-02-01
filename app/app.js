39976383617a10ee26a9dd3b0955a96f3cd8633a
T-Farmer <terrill@Rels-Pro-186.local>
2017-02-01 10:05:27 -0600

    firebase init and routing structure

--- app/app.js
+++ app/app.js
@@ -4,6 +4,20 @@ const app = angular.module('redditApp', ['ngRoute'])
 
 app.config(($routeProvider, $locationProvider) => {
   $locationProvider.hashPrefix('')
+
+  // Initialize Firebase
+
+  var config = {
+    apiKey: "AIzaSyAtF6bMtPEHRtwgHBVlH9lhgZubrn-yiO0",
+    authDomain: "have-you-reddit.firebaseapp.com",
+    databaseURL: "https://have-you-reddit.firebaseio.com",
+    storageBucket: "have-you-reddit.appspot.com",
+    messagingSenderId: "1077173442347"
+  };
+  firebase.initializeApp(config);
+
+  // routing
+
   $routeProvider
     .when('/', {
       controller: 'HomeCtrl',
@@ -21,3 +35,15 @@ app.config(($routeProvider, $locationProvider) => {
       templateUrl: '/partials/newpost.html'
     })
 })
+
+// authorization
+
+  checkForAuth ($location) {
+        // http://stackoverflow.com/questions/37370224/firebase-stop-listening-onauthstatechanged
+        const authReady = firebase.auth().onAuthStateChanged(user => {
+          authReady()
+          if (!user) {
+            $location.url('/login')
+          }
+        })
+      }
