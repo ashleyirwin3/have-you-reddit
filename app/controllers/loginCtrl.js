a470be8c1e8b0ac745a7f51a8b0ecc7d85c95a16
T-Farmer <terrill@Rels-Pro-186.local>
2017-02-01 11:08:36 -0600

    loginCtrl js

---
+++ app/controllers/loginCtrl.js
@@ -0,0 +1,9 @@
+app.controller("LoginCtrl", function($scope, $location, AuthFactory){
+  $scope.login = () =>  AuthFactory
+  .login($scope.email, $scope.password)
+    .then(() => {
+      $location.url("/");
+      $scope.$apply();
+    });
+
+  });
