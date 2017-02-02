7c4c8e0e4c7fcc3b784d8ce82dd566f78c04218b
T-Farmer <terrillfarmer@gmail.com>
2017-02-02 09:53:15 -0600

    new post route works

--- app/controllers/newpost.js
+++ app/controllers/newpost.js
@@ -1,3 +1,6 @@
-console.log('new post controller')
-app.controller('NewpostCtrl', function($scope) {
+console.log('new post controller');
+
+app.controller('NewpostCtrl', function($scope, $q, $location) {
+  .then(() => {
+        $location.url('/newpost')
 })
